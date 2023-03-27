44eb211a73062d2fe615957907e2dc2beffa737a
Jim <jim_f_d@hotmail.com>
2023-03-27 12:21:44 +0200

    random quote

--- myapp/app.js
+++ myapp/app.js
@@ -2,14 +2,12 @@
 import path from 'path'
 import express from 'express'
 import handlebars from 'express-handlebars'
-import router from './routes/router.js'
 
 const app = express()
 const port = 3000
 const __dirname = path.resolve();
 
 app.use(express.static('public'))
-app.use('/', router)
 
 app.set('view engine', 'hbs')
 app.set('views', 'views')
@@ -22,11 +20,25 @@ app.engine('hbs', handlebars.engine({
 }))
 
 app.get('/', (req, res) => {
-  res.render('main', {layout: 'index'})
+  res.render('home', {layout: 'index'})
 })
 
-app.get('/quotes', (req, res) => {
-  // res.render('quotes', {layout: 'index'})
+app.get('/quotes', async (req, res) => {
+  const response = await fetch('https://opensheet.elk.sh/1NNaZeJXR-AaBeRoIrphPCTeAx1ltZ4ltH0yGV9_WIQ0/quotes')
+   const data = await response.json();
+   console.log(data);
+   res.render('quotes', {quotes: data})
+})
+
+app.get('/randomquote', async (req, res) => {
+  const response = await fetch('https://opensheet.elk.sh/1NNaZeJXR-AaBeRoIrphPCTeAx1ltZ4ltH0yGV9_WIQ0/quotes')
+   const data = await response.json();
+
+   const randomIndex = Math.floor(Math.random() * data.length);
+   const randomQuote = data[randomIndex];
+
+
+   res.render('randomquote', {author: randomQuote.author, text: randomQuote.text});
 })
 
 app.listen(port, () => {
